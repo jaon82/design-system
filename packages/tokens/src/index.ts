df92a19270d887d9e768bafefc9ce622300b94b2
Jean Nunes <jean.nunes@uzzipay.com>
2024-06-14 19:45:42 -0400

    Add ESLint

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,8 +1,8 @@
-export * from "./colors";
-export * from "./radii";
-export * from "./space";
+export * from './colors'
+export * from './radii'
+export * from './space'
 
-export * from "./font-sizes";
-export * from "./font-weights";
-export * from "./fonts";
-export * from "./line-heights";
+export * from './font-sizes'
+export * from './font-weights'
+export * from './fonts'
+export * from './line-heights'
