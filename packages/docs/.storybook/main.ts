2645bab0c435ae7bf4012279500b20a0b789330c
Jean Nunes <jean.nunes@uzzipay.com>
2024-06-18 17:46:45 -0400

    Update deploy

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -26,12 +26,5 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/design-system/'
-    }
-
-    return config
-  }
 };
 export default config;
